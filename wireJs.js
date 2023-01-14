12a9223678a94c8e310fa12799daceb7b65c5274
acostavs <acostavs1015@gmail.com>
2023-01-14 00:54:06 +0000

    added more conditionals

--- wireJs.js
+++ wireJs.js
@@ -1,20 +1,33 @@
 function greetUser() {
-    confirm("Are The Cowboys The Best Football Team?")
     let getUserName = prompt("What's your name?");
     console.log("You wrote " + getUserName);
     document.write("Welcome Back " + getUserName + " ");
 };
 
-greetUser();
-let myFavTeam = "Cowboys";
-let yourGuess = prompt("Who's The Best Football Teamm?");
+function getTeam() {
+    let getTeam = prompt("Who The Best NFC East Team?");
+    if (getTeam == "Cowboys") {
+        document.write("GO COWBOYS!!!")
 
-if (yourGuess == myFavTeam) {
-    document.write("GO COWBOYS!!!")
+    }
+    else {
+        alert("BOOOOOOOOOO!!!!!")
+    }
+
 }
-else if (yourGuess == "Commanders") {
-    document.write("BOOOOOO!!")
-}
-else {
-    document.write("Wrong guess")
-};
+
+greetUser();
+getTeam();
+
+// let myFavTeam = "Cowboys";
+// let yourGuess = prompt("Who's The Best Football Team?");
+
+// if (yourGuess == myFavTeam) {
+//     document.write("GO COWBOYS!!!")
+// }
+// else if (yourGuess == "Commanders") {
+//     document.write("BOOOOOO!!")
+// }
+// else {
+//     document.write("Wrong guess")
+// };
